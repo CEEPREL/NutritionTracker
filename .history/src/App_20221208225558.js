f9f6cd4c68936182e8bfd644e49f97c4a257f370
Vasil <vaskodzhakov@gmail.com>
2022-12-09 11:09:53 +0000

    Add buttons added
    Add buttons added

---
+++ .history/src/App_20221208225558.js
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import './styles/searchFood.css';
+import data from './data';
+
+import CalculateNutrients from './components/CalculateNutrients';
+import FoodList from './components/FoodList';
+import Header from './components/Header';
+
+const App = () => {
+
+  
+
+  const { food_data } = data;
+  return (
+    <div className="container">
+      <Header />
+
+      <div className="wrapper-card">
+        <h3>Wednesday, 7th December 2022</h3>
+      </div>
+
+      <div className="row">
+
+        <div className="col-left">
+          <div className="wrapper-card">
+            <FoodList food_data={food_data} />
+          </div>
+        </div>
+
+        <div className="col-right">
+          <div className="wrapper-card">
+            <CalculateNutrients />
+          </div>
+
+          <div className="wrapper-card">
+            Selected Item info
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ .history/src/components/Food_20221209020428.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Food = ({ id, name, size, protein, image }) => {
+    return (
+        <div className="food-card">
+            <div className="food-name">
+                <h4 ><strong>{name}</strong></h4>
+            </div>
+            
+            <img className="small" src={image} alt={name}></img>
+            
+            <div className="food-nutrients">
+                <p>Serving: {size}</p>
+                <p>Protein: {protein}</p>
+            </div>
+            <button onClickclassName="add-btn">Add</button>
+
+        </div>
+    )
+}
+
+Food.defaultProps = {
+    name: "Enter Food",
+    serving: "0g",
+    protein: "0g"
+}
+
+export default Food
