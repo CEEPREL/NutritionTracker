e0172c3154f439a41ea8e4b648b4249fa926fca5
Vasil <vaskodzhakov@gmail.com>
2022-12-18 15:45:08 +0000

    Modal and fetch food api
    Implemented react-modal to add new custom food, and added fetch food api from USDA foods for users to look up food and display them

---
+++ .history/src/fetchFoodApi_20221218135042.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+import React from 'react'
+
+export default function FetchFoodApi() {
+    const API_KEY = 'XlbmfvBoP5wat3i76hIV0RZzstoaPCp9M52WGasf'
+    const [apiFood, setApiFood] = useState();
+
+    useEffect(() => {
+        fetch()
+    }, []);
+
+    return (
+        <div>fetchFoodApi</div>
+    )
+}
+
+// https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=XlbmfvBoP5wat3i76hIV0RZzstoaPCp9M52WGasf
---
+++ .history/src/fetchFoodApi_20221218145501.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+import React from 'react'
+
+export default function FetchFoodApi() {
+    const API_KEY = 'XlbmfvBoP5wat3i76hIV0RZzstoaPCp9M52WGasf'
+    const [apiData, setApiData] = useState(null);
+    const [typedFood, setTypedFood] = useState('');
+    const [fetchedFood, setFetchedFood] = useState('');
+
+    function fetchFoodData(typedFood) {
+        fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${API_KEY}&${typedFood}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setFetchedFood(data);
+            }.catch ((err) => {
+                console.log(err.message);
+            }
+        );
+            }
+        useEffect(() => {
+            fetchFoodData();
+        }, []);
+
+        return (
+
+            <div className="container">
+                <div className="wrapper-card">
+                    <button onClick={openModal}>Open Modal</button>
+                    <Modal
+                        isOpen={modalIsOpen}
+                        onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal" >
+                        <div className="modal-title">
+                            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+                            <button onClick={closeModal}>close</button>
+                        </div>
+
+                        <div>I am a modal</div>
+                        <form>
+                            <input />
+
+                        </form>
+                    </Modal>
+                    <button onClick={() => { fetchFoodData(typedFood) }}></button>
+                </div>
+
+                
+            </div>
+        )
+    }
+
+// https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=XlbmfvBoP5wat3i76hIV0RZzstoaPCp9M52WGasf
